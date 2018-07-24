d5b9831e9420049c6c0d73121a8ca3f3b1ed70ac
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-07-24 18:08:28 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Queue.h>
+
+int main() {
+		Queue<int> queue;
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+
+		queue.enqueue(4);
+		queue.dequeue();
+		queue.enqueue(5);
+		queue.dequeue();
+		queue.show();
+}
